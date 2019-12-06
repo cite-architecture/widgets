8674bc60eff22d30653cbd236dc0e1725336897c
Eumaeus <cwblackwell@gmail.com>
2019-12-06 14:51:50 -0500

    urn_toggle works for CTS URNs.

--- urn_toggle/cite_urn_toggle.js
+++ urn_toggle/cite_urn_toggle.js
@@ -1,48 +1,45 @@
-/*
-
-		We start with just "urn"
-		"passage"
-		"medium"
-		"full"
-
-
-*/
-
-function ctsMinimal() {
-	return "urn"
-}
-
-function ctsPassage( us ) {
-	var u = new CtsUrn( { urnString: us });
-	var passage = u.passageComponent;
-	var returnVal = passage;
-	if (returnVal == "") {
-		return "no passage";
-	} else {
-		return returnVal;
-	}
-}
-
-function ctsMedium( us ) {
-	return us.replace(/urn:cts:[^:]+:/,"");
-}
-
 
 $(document).ready(function () {
 
 	// make all minimal
-	
+	$(".ctsurn").removeClass("ctsUrn0");
+	$(".ctsurn").removeClass("ctsUrn1");
+	$(".ctsurn").removeClass("ctsUrn2");
+	$(".ctsurn").removeClass("ctsUrn3");
+	$(".ctsurn").addClass("ctsUrn0");
+	$(".ctsurn").html("cts-urn");
+
 
 	$(".ctsurn").click(function(){ 
 		var urnString = $(this).attr("data-ctsurn");
-		if ( $(this).hasClass("displayFullUrn") ) {
-			console.log("Will display minimal");
-		} else if ( $(this).hasClass("displayMediumUrn") ) {
-			console.log("Will display full urn");
-		} else if ( $(this).hasClass("displayUrnPassage") ) {
-			console.log("Will display medium urn");
+		if ( $(this).hasClass("ctsUrn0") ) {
+			// passage (ctsUrn1)
+			$(this).removeClass("ctsUrn0");
+			$(this).addClass("ctsUrn1");
+			var u = new edu.holycross.shot.cite.CtsUrn( { urnString: urnString });
+			var passage = u.passageComponent;
+			if (passage == "") {
+				$(this).removeClass("ctsUrn1");
+				$(this).addClass("ctsUrn2");
+				$(this).html(us.replace(/urn:cts:[^:]+:/,""));
+			} else {
+				$(this).html(passage);
+			}
+		} else if ( $(this).hasClass("ctsUrn1") ) {
+			// biblio+passage (ctsUrn2)
+			$(this).removeClass("ctsUrn1");
+			$(this).addClass("ctsUrn2");
+			$(this).html(urnString.replace(/urn:cts:[^:]+:/,""));
+		} else if ( $(this).hasClass("ctsUrn2") ) {
+			// full URN (ctsurn3)
+			$(this).removeClass("ctsUrn2");
+			$(this).addClass("ctsUrn3");
+			$(this).html(urnString);
 		} else {
-			console.log("Will display passage");
+			// just "urn" (ctsUrn0)
+			$(this).removeClass("ctsUrn3");
+			$(this).addClass("ctsUrn0");
+			$(this).html("cts-urn");
 		}
 
 	});
