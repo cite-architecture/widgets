1fec4315f3b5898ff1057f776da0c016d6b14a11
Eumaeus <cwblackwell@gmail.com>
2019-12-06 22:22:30 -0500

    urn_toggle works.

--- urn_toggle/cite_urn_toggle.js
+++ urn_toggle/cite_urn_toggle.js
@@ -8,7 +8,28 @@ $(document).ready(function () {
 	$(".ctsurn").removeClass("ctsUrn3");
 	$(".ctsurn").addClass("ctsUrn0");
 	$(".ctsurn").html("cts-urn");
+	$(".cite2urn").removeClass("cite2Urn0");
+	$(".cite2urn").removeClass("cite2Urn1");
+	$(".cite2urn").removeClass("cite2Urn2");
+	$(".cite2urn").removeClass("cite2Urn3");
+	$(".cite2urn").addClass("cite2Urn0");
+	$(".cite2urn").html("cite2-urn");
 
+	// Add a tooltip to each URN object
+	$(".ctsurn").wrap("<div class='tooltip'></div>");
+	$(".cite2urn").wrap("<div class='tooltip'></div>");
+	$(".tooltip").append("<div class='top'><p>double-click to expand</p><i></i></div>");
+
+
+/*
+<div class="btn btn-primary tooltip">Hover Me to Preview
+    <div class="top">
+        <h3>Lorem Ipsum</h3>
+        <p>Dolor sit amet, consectetur adipiscing elit.</p>
+        <i></i>
+    </div>
+</div>
+*/
 
 	$(".ctsurn").dblclick(function(){ 
 		var urnString = $(this).attr("data-ctsurn");
@@ -44,6 +65,39 @@ $(document).ready(function () {
 
 	});
 
+$(".cite2urn").dblclick(function(){ 
+		var urnString = $(this).attr("data-cite2urn");
+		if ( $(this).hasClass("cite2Urn0") ) {
+			// passage (cite2Urn1)
+			$(this).removeClass("cite2Urn0");
+			$(this).addClass("cite2Urn1");
+			var u = new edu.holycross.shot.cite.Cite2Urn( { urnString: urnString });
+			var passage = u.objectComponent;
+			if (passage == "") {
+				$(this).removeClass("cite2Urn1");
+				$(this).addClass("cite2Urn2");
+				$(this).html(us.replace(/urn:cite2:[^:]+:/,""));
+			} else {
+				$(this).html(passage);
+			}
+		} else if ( $(this).hasClass("cite2Urn1") ) {
+			// biblio+passage (cite2Urn2)
+			$(this).removeClass("cite2Urn1");
+			$(this).addClass("cite2Urn2");
+			$(this).html(urnString.replace(/urn:cite2:[^:]+:/,""));
+		} else if ( $(this).hasClass("cite2Urn2") ) {
+			// full URN (ctsurn3)
+			$(this).removeClass("cite2Urn2");
+			$(this).addClass("cite2Urn3");
+			$(this).html(urnString);
+		} else {
+			// just "urn" (cite2Urn0)
+			$(this).removeClass("cite2Urn3");
+			$(this).addClass("cite2Urn0");
+			$(this).html("cite2-urn");
+		}
+
+	});
 
 });
 
